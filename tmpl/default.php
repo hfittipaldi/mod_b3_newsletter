f89d908e189b5fb36826bb7a37dd104657b2e733
Hugo Fittipaldi <hugo.fittipaldi@gmail.com>
2016-10-16 15:21:29 -0200

    Remove  the 'for' attribute of the labels.

--- tmpl/default.php
+++ tmpl/default.php
@@ -18,11 +18,11 @@ defined('_JEXEC') or die;
 <form action="<?php echo JRoute::_('index.php'); ?>" method="post" role="form" class="b3Newsletter">
     <fieldset><?php if ($pre_text != '') { ?><legend class="modnsintro"><?php echo $pre_text; ?></legend><?php } ?>
         <div class="form-group">
-            <label for="m_name<?php echo $unique_id; ?>" class="sr-only"><?php echo $myNameLabel; ?></label>
+            <label class="sr-only"><?php echo $myNameLabel; ?></label>
             <input type="text" class="form-control" name="m_name<?php echo $unique_id; ?>" size="50" value="<?php echo $session->get('subscriberName'); ?>" placeholder="Nome completo" required />
         </div>
         <div class="form-group">
-            <label for="m_email<?php echo $unique_id; ?>" class="sr-only"><?php echo $myEmailLabel; ?></label>
+            <label class="sr-only"><?php echo $myEmailLabel; ?></label>
             <input type="email" class="form-control" name="m_email<?php echo $unique_id; ?>" value="<?php echo $session->get('subscriberEmail'); ?>" size="50" placeholder="Email" required />
         </div>
 
